ed661295c1feebc53dad60551bcf2bee906de5f9
Anindra <anindrakarmakar@proton.me>
2022-11-04 18:02:41 +0530

    first commit(Docker file added)
    Docker file added with original file and then comited

---
+++ src/main/java/com/codeavenge/krishibikashapi/KrishiBikashApiApplication.java
@@ -0,0 +1,13 @@
+package com.codeavenge.krishibikashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrishiBikashApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrishiBikashApiApplication.class, args);
+    }
+
+}
